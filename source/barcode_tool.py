2d2d80fc08119e19d9745b018502a11aedf45cda
aariani <aariani@ucdavis.edu>
2016-05-10 17:23:51 -0700

    Other

--- source/barcode_tool.py
+++ source/barcode_tool.py
@@ -2,7 +2,7 @@
 from __future__ import print_function
 import gzip ## just in case the --gz param in the main file is active
 
-def getBcInfo(bcfile, w, suffix):
+def getBcInfo(bcfile, suffix, gz):
 ### Get barcodes infos, open files accordingly to the Genotypes available in the barcode (and the gz option)
 ### Return a barcode dictionary and the longer barcode (useful for further demultiplexing), and a dictionary used for demultiplexing stats
 	barcode_d={}
